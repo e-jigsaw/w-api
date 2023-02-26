6e2351fcdb4663227e159bb856799c7233cfe812
jigsaw <m@jgs.me>
2023-02-26 11:51:41 +0900

    add clips

--- mod.ts
+++ mod.ts
@@ -23,6 +23,25 @@ app.get("/latest_articles", async ({ req, ...c }) => {
     payload: data,
   });
 });
+app.get("/clips", async ({ req, ...c }) => {
+  const url = new URL(req.url);
+  const p = parseInt(url.searchParams.get("p") ?? "0");
+  const { data, error } = await client.rpc("latest_clips", {
+    offset_arg: p * 20,
+    limit_arg: (p + 1) * 20,
+  });
+  if (error) {
+    console.error(error);
+    c.status(500);
+    return c.json({
+      ok: false,
+    });
+  }
+  return c.json({
+    ok: true,
+    payload: data,
+  });
+});
 app.get("/recent_articles", async (c) => {
   const { data, error } = await client.rpc("recent_articles");
   if (error) {
